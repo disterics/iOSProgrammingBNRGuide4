b7744177e83d5ca6a7ce95965e93478cba12a961
disterics <disterics@wojeshun.net>
2015-07-28 22:04:15 -0700

    Completed the Quiz App in Chapter 1

--- Quiz/Quiz/AppDelegate.m
+++ Quiz/Quiz/AppDelegate.m
@@ -2,11 +2,12 @@
 //  AppDelegate.m
 //  Quiz
 //
-//  Created by Henry Barnor on 7/27/15.
+//  Created by disterics on 7/27/15.
 //  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
 //
 
 #import "AppDelegate.h"
+#import "QuizViewController.h"
 
 @interface AppDelegate ()
 
@@ -18,6 +19,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+
+    QuizViewController *quizVC = [[QuizViewController alloc] init];
+    self.window.rootViewController = quizVC;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
--- Quiz/Quiz/main.m
+++ Quiz/Quiz/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  Quiz
 //
-//  Created by Henry Barnor on 7/27/15.
+//  Created by disterics on 7/27/15.
 //  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
 //
 

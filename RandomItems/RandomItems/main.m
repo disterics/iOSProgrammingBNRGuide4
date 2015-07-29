a413767c0c88681b1103160997fa9bb6212ae1bd
disterics <disterics@wojeshun.net>
2015-07-28 22:30:10 -0700

    Completed the first part of chapter 2.
    Create a populated NSMutableArray and print it out.

--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -10,8 +10,25 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc] init];
+        
+        // Send the message addObject: to the NSMutableArray pointed to
+        // by the variable items, passing a string each time
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        //Send another message insertObject:atIndex:, to that same array object
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // Print items in the array
+        for (NSString *item in items) {
+            // Log the description of item
+            NSLog(@"%@", item);
+        }
+        // Destory the mutable array object
+        items = nil;
     }
     return 0;
 }

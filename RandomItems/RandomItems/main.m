bcb09ce0718a4ee5fb76b8bc9e69edadd2f642af
disterics <disterics@wojeshun.net>
2015-07-28 22:20:22 -0700

    Created RandomItems commandline project

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by disterics on 7/28/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

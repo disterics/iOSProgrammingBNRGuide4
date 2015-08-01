37837f6bc1e158c2762e14d7866db67b809b6602
disterics <disterics@wojeshun.net>
2015-08-01 12:27:23 -0700

    Use class methods to create random instances of BNRItem
    Added a class method that creates a random instance of BNRItem.
    Used that to populate the array of items in main.

--- RandomItems/RandomItems/BNRItem.m
+++ RandomItems/RandomItems/BNRItem.m
@@ -10,6 +10,31 @@
 
 @implementation BNRItem
 
++ (instancetype)randomItem
+{
+    // Create an immutable array of three adjectives
+    NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
+    // Create an immutable array of three nouns
+    NSArray *randomNounList = @[@"Bear", @"Spork", @"Mac"];
+    // get the index of random adjective/noun from the lists
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    // NSInteger is a type definition and not an object
+    
+    NSString *randomName = [NSString stringWithFormat:@"%@ %@", [randomAdjectiveList objectAtIndex:adjectiveIndex],
+                            [randomNounList objectAtIndex:nounIndex]];
+    
+    int randomValue = arc4random() % 100;
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c",
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10,
+                                    'A' + arc4random() % 26,
+                                    '0' + arc4random() % 10];
+    BNRItem *newItem = [[self alloc] initWithItemName:randomName valueInDollars:randomValue serialNumber:randomSerialNumber];
+    return newItem;
+}
+
 - (instancetype)initWithItemName:(NSString *)name
                   valueInDollars:(int)value
                     serialNumber:(NSString *)sNumber
--- RandomItems/RandomItems/main.m
+++ RandomItems/RandomItems/main.m
@@ -14,30 +14,17 @@ int main(int argc, const char * argv[]) {
         // Create a mutable array object, store its address in items variable
         NSMutableArray *items = [[NSMutableArray alloc] init];
         
-        // Send the message addObject: to the NSMutableArray pointed to
-        // by the variable items, passing a string each time
-        [items addObject:@"One"];
-        [items addObject:@"Two"];
-        [items addObject:@"Three"];
-        
-        //Send another message insertObject:atIndex:, to that same array object
-        [items insertObject:@"Zero" atIndex:0];
-        
+        for (int i = 0; i < 10; ++i)
+        {
+            BNRItem *item = [BNRItem randomItem];
+            [items addObject:item];
+        }
         // Print items in the array
-        for (NSString *item in items) {
+        for (BNRItem *item in items) {
             // Log the description of item
             NSLog(@"%@", item);
         }
         
-        BNRItem *item = [[BNRItem alloc] initWithItemName:@"Red Sofa" valueInDollars:100 serialNumber:@"A1B2C"];
-        NSLog(@"%@", item);
-        
-        BNRItem *itemWithName = [[BNRItem alloc] initWithItemName:@"Blue Sofa"];
-        NSLog(@"%@", itemWithName);
-
-        BNRItem *itemWithNoName = [[BNRItem alloc] init];
-        NSLog(@"%@", itemWithNoName);
-
         // Destory the mutable array object
         items = nil;
     }

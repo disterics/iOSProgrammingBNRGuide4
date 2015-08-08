57434888b533d0097a5525a657418fe09538a5b3
disterics <disterics@wojeshun.net>
2015-08-07 18:19:59 -0700

    Use UIBezierPath to draw concentric circles in a view.

--- Hypnosister/Hypnosister/BNRHypnosisView.m
+++ Hypnosister/Hypnosister/BNRHypnosisView.m
@@ -10,12 +10,46 @@
 
 @implementation BNRHypnosisView
 
-/*
+- (instancetype) initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        // always start with a clear background
+        self.backgroundColor = [UIColor clearColor];
+    }
+    return self;
+}
+
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (void)drawRect:(CGRect)rect
+{
+    CGRect bounds = self.bounds;
+    //Center of the screen
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    // largest circle will circumscribe the view
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20)
+    {
+        // make sure circle starts somewhere n the circle
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        // add an arc at the center with radius from 0 to 2PI radians (a circle)
+        [path addArcWithCenter:center radius:currentRadius startAngle:0 endAngle:M_PI * 2.0 clockwise:YES];
+    }
+    // configure the line width
+    path.lineWidth = 10;
+    // configure the stroke color to light grey
+    [[UIColor lightGrayColor] setStroke];
+    // draw the circle
+    [path stroke];
 }
-*/
+
 
 @end

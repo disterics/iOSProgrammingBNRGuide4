413670d8d1e801809367da003286de7c87827681
disterics <disterics@wojeshun.net>
2015-08-07 17:45:19 -0700

    More experiments with view hierarchies.
    View hierarchies can be more than one level deep

--- Hypnosister/Hypnosister/AppDelegate.m
+++ Hypnosister/Hypnosister/AppDelegate.m
@@ -23,6 +23,10 @@
     BNRHypnosisView *firstView = [[BNRHypnosisView alloc] initWithFrame:firstFrame];
     firstView.backgroundColor = [UIColor redColor];
     [self.window addSubview:firstView];
+    CGRect secondFrame = CGRectMake(20, 30, 50, 50);
+    BNRHypnosisView *secondView = [[BNRHypnosisView alloc] initWithFrame:secondFrame];
+    secondView.backgroundColor = [UIColor blueColor];
+    [firstView addSubview:secondView];
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;

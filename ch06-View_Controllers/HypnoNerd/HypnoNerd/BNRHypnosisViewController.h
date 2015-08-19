5d2cf7ff38e0ca97c48065ee0092230badea59f2
disterics <disterics@wojeshun.net>
2015-08-18 21:22:41 -0700

    Empty HypnoNerd application with a ViewController class.

---
+++ ch06-View_Controllers/HypnoNerd/HypnoNerd/BNRHypnosisViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.h
+//  HypnoNerd
+//
+//  Created by disterics on 8/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisViewController : UIViewController
+
+@end
---
+++ ch06-View_Controllers/HypnoNerd/HypnoNerd/BNRHypnosisViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisViewController.m
+//  HypnoNerd
+//
+//  Created by disterics on 8/18/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRHypnosisViewController.h"
+
+@implementation BNRHypnosisViewController
+
+@end

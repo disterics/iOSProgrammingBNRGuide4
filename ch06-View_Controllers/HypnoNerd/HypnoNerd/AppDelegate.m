8b5fd3e2596d2a290b7dd8afee0035a211b34f98
disterics <disterics@wojeshun.net>
2015-08-21 12:52:10 -0700

    Learnt about using xib files as the view of a view controller.
    Created an empty xib file and populated it with views.
    Used the FileOwner placeholder object to connect the xib to the
    BNRRemindViewController class. Used the initWithNibName to load
    the xib file at runtime.

--- ch06-View_Controllers/HypnoNerd/HypnoNerd/AppDelegate.m
+++ ch06-View_Controllers/HypnoNerd/HypnoNerd/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 #import "BNRHypnosisViewController.h"
+#import "BNRReminderViewController.h"
 
 @interface AppDelegate ()
 
@@ -20,7 +21,14 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     BNRHypnosisViewController *hvc = [[BNRHypnosisViewController alloc] init];
-    self.window.rootViewController = hvc;
+    
+    // This will get a pointer to an object that represents the app bundle
+    NSBundle *appBundle = [NSBundle mainBundle];
+    
+    // Look in the appBundle for the BNRReminderViewController.xib
+    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] initWithNibName:@"BNRReminderViewController"
+                                                                                 bundle:appBundle];
+    self.window.rootViewController = rvc;
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;

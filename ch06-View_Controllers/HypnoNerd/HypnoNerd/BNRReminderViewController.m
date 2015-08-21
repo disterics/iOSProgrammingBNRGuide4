d392feb0c3900759616aa727feb67ebc2f84fa89
disterics <disterics@wojeshun.net>
2015-08-21 11:34:42 -0700

    Create a secondary UIViewController

---
+++ ch06-View_Controllers/HypnoNerd/HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by disterics on 8/21/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end

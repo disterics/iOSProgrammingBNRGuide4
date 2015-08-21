b6c499ad4a9b247a490de6a36eeb3f47855b3fc4
disterics <disterics@wojeshun.net>
2015-08-21 13:23:52 -0700

    Learnt about how to get the icons and names for TabbedBarController
    Overrode the initializers for my two ViewControllers to configure
    their tabBarItems with a name and an image.

--- ch06-View_Controllers/HypnoNerd/HypnoNerd/BNRReminderViewController.m
+++ ch06-View_Controllers/HypnoNerd/HypnoNerd/BNRReminderViewController.m
@@ -17,6 +17,26 @@
 
 @implementation BNRReminderViewController
 
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self)
+    {
+        // set the tab item's title
+        self.tabBarItem.title = @"Reminder";
+        
+        // create a UIImage from a file
+        // will use Hypno@2x.png for retina display devices
+        UIImage *image = [UIImage imageNamed:@"Time.png"];
+        
+        // put the image on the tab bar item
+        self.tabBarItem.image = image;
+    }
+    return self;
+}
+
+
 - (IBAction)addReminder:(id)sender
 {
     NSDate *date = self.datePicker.date;

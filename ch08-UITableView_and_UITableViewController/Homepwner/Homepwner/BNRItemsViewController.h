a3bc2b844242e89cbc3aaf75903e7108e40c6f9f
disterics <disterics@wojeshun.net>
2015-08-25 22:48:40 -0700

    Added empty view controller

---
+++ ch08-UITableView_and_UITableViewController/Homepwner/Homepwner/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner
+//
+//  Created by disterics on 8/25/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItemsViewController : NSObject
+
+@end
---
+++ ch08-UITableView_and_UITableViewController/Homepwner/Homepwner/BNRItemsViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner
+//
+//  Created by disterics on 8/25/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+@end

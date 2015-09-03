a8437b00fd7a6de03a2fac929f4dbd488d8bf372
disterics <disterics@wojeshun.net>
2015-09-03 10:08:30 -0700

    Learnt about implementing UITableViewDataSource protocol
    Implemented the required methods of the UITableViewDataSource protocol.
    Optimized the UITableViewCell creation method to reuse existing
    UITableViewCells that are not currently being displayed.

--- ch08-UITableView_and_UITableViewController/Homepwner/Homepwner/BNRItemsViewController.m
+++ ch08-UITableView_and_UITableViewController/Homepwner/Homepwner/BNRItemsViewController.m
@@ -8,6 +8,9 @@
 
 #import "BNRItemsViewController.h"
 
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
 @implementation BNRItemsViewController
 
 - (instancetype) init
@@ -19,7 +22,42 @@
 {
     // Call the superclass's designated initializer
     self = [super initWithStyle:UITableViewStylePlain];
+    if (self)
+    {
+        for (int i = 0; i < 5; ++i)
+        {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
     return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"UITableViewCell"];
+}
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instance of UITableViewCell, with default appearance
+    // UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
+    // Get a new or recycled cell
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" forIndexPath:indexPath];
+    // ste the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the table view
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *item = items[indexPath.row];
+    
+    cell.textLabel.text = [item description];
+    
+    return cell;
 }
 
 @end

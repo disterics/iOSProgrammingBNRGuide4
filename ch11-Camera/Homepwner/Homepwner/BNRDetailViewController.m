1411ed83f11237891539f4b10e54fa97b0d3f3bc
disterics <disterics@wojeshun.net>
2015-09-03 16:51:11 -0700

    Creating a copy of Homepwner from chapter 10 for chapter 11

---
+++ ch11-Camera/Homepwner/Homepwner/BNRDetailViewController.m
@@ -0,0 +1,64 @@
+//
+//  BNRDetailViewController.m
+//  Homepwner
+//
+//  Created by disterics on 9/3/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+#import "BNRItem.h"
+
+@interface BNRDetailViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
+@end
+
+@implementation BNRDetailViewController
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    BNRItem *item = [self item];
+    
+    self.nameField.text = item.itemName;
+    self.serialNumberField.text = item.serialNumber;
+    self.valueField.text = [NSString stringWithFormat:@"%d", item.valueInDollars];
+    
+    static NSDateFormatter *dateFormatter;
+    if (!dateFormatter)
+    {
+        dateFormatter = [[NSDateFormatter alloc] init];
+        dateFormatter.dateStyle = NSDateFormatterMediumStyle;
+        dateFormatter.timeStyle = NSDateFormatterNoStyle;
+    }
+    
+    // use a filtered NSDate object to set the dateLabel contents
+    self.dateLabel.text = [dateFormatter stringFromDate:item.dateCreated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    
+    // clear first responder
+    [self.view endEditing:YES];
+    
+    // save changes to the item
+    BNRItem *item = self.item;
+    item.itemName = self.nameField.text;
+    item.serialNumber = self.serialNumberField.text;
+    item.valueInDollars = [self.valueField.text intValue];
+}
+
+-(void)setItem:(BNRItem *)item
+{
+    _item = item;
+    self.navigationItem.title = _item.itemName;
+}
+@end

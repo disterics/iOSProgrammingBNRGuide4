316e8da619f330fd27e7d144caf7523f748c15a6
disterics <disterics@wojeshun.net>
2015-09-04 10:59:57 -0700

    Creating a copy of Homepwner from chapter 15 for chapter 16

---
+++ ch16-Auto_Layout:Programmatic_Constraints/Homepwner/Homepwner/BNRDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.h
+//  Homepwner
+//
+//  Created by disterics on 9/3/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BNRItem;
+
+@interface BNRDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end

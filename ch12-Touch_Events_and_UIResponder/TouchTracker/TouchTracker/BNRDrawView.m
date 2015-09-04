a71def4853d800116a1f6c97aeecda7eb4d5f3a1
disterics <disterics@wojeshun.net>
2015-09-03 19:30:33 -0700

    Implement drawing in the BNRDrawView class

--- ch12-Touch_Events_and_UIResponder/TouchTracker/TouchTracker/BNRDrawView.m
+++ ch12-Touch_Events_and_UIResponder/TouchTracker/TouchTracker/BNRDrawView.m
@@ -8,6 +8,55 @@
 
 #import "BNRDrawView.h"
 
+#import "BNRLine.h"
+
+@interface BNRDrawView ()
+
+@property (nonatomic, strong) BNRLine *currentLine;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
+
 @implementation BNRDrawView
+
+- (instancetype)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+    
+    if (self)
+    {
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    // Draw the finished lines in black
+    [[UIColor blackColor] set];
+    for (BNRLine *line in self.finishedLines)
+    {
+        [self strokeLine:line];
+    }
+    
+    if (self.currentLine)
+    {
+        // draw the current line in red
+        [[UIColor redColor] set];
+        [self strokeLine:self.currentLine];
+    }
+}
 
 @end

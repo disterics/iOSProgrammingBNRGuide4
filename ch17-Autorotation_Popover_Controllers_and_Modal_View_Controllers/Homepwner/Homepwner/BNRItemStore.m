6ad273516e9d23a47eb826d26e53a180e5bb2e93
disterics <disterics@wojeshun.net>
2015-09-04 14:14:15 -0700

    Learnt about ho wto implement thread-safe singletons

--- ch17-Autorotation_Popover_Controllers_and_Modal_View_Controllers/Homepwner/Homepwner/BNRItemStore.m
+++ ch17-Autorotation_Popover_Controllers_and_Modal_View_Controllers/Homepwner/Homepwner/BNRItemStore.m
@@ -22,10 +22,10 @@
 + (instancetype)sharedStore
 {
     static BNRItemStore *sharedStore;
-    if (!sharedStore)
-    {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
         sharedStore = [[self alloc] initPrivate];
-    }
+    });
     return sharedStore;
 }
 

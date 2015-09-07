fc3af3fc82dab9b1048867c4ba8b8370b43ffdfd
disterics <disterics@wojeshun.net>
2015-09-06 23:08:29 -0700

    Create an app and add a storyboard to it

---
+++ ch28-UIStoryboard/Colorboard/Colorboard/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Colorboard
+//
+//  Created by disterics on 9/6/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
